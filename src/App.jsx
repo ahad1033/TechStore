de89ee70e7fda62ffbfaa263155ee88f3d812aa4
ahad1033 <a.ahad2232@gmail.com>
2025-06-29 07:27:08 +0600

    Created folder structures and component structures with initialized shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <h1 class="text-7xl font-bold underline">Hello world!</h1>
+      <Button variant="destructive">hello</Button>
     </>
   );
 }
